fdc3552cc38bd0b7a515f6256ee2db63cff714c3
xby <xby@qq.com>
2022-04-09 23:14:30 +0800

    third commit

--- src/Test2.java
+++ src/Test2.java
@@ -1,8 +1,8 @@
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("hello.git2");
-        System.out.println("hello.git2");
-        System.out.println("hello.git2");
-        System.out.println("hello.git2");
+        System.out.println("hello.git3");
+        System.out.println("hello.git3");
+        System.out.println("hello.git3");
+        System.out.println("hello.git3");
     }
 }
