3f8a3a5fb83aad68cbc2262d1ea1a70e313f17e7
xby <xby@qq.com>
2022-04-09 23:13:35 +0800

    second commit

--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,8 @@
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("hello.git");
+        System.out.println("hello.git2");
+        System.out.println("hello.git2");
+        System.out.println("hello.git2");
+        System.out.println("hello.git2");
     }
 }
