1eb01669a1618aca307a2596e6c00ce469f39b11
xby <xby@qq.com>
2022-04-10 09:12:44 +0800

    hot-fix commit2

--- src/Test2.java
+++ src/Test2.java
@@ -5,5 +5,6 @@ public class Test2 {
         System.out.println("hello.git3");
         System.out.println("hello.git3");
         System.out.println("hello.git4");
+        System.out.println("hello git hot-fix");
     }
 }
